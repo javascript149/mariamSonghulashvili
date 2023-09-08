a1a1cd2428fc4a872aee57c1e7b5219310e5d81c
mariamsonghula <songhulashvilim@gmail.com>
2023-09-08 20:32:00 +0400

    Homework 1
    Pirveli nawili

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let arrayOfNames = ["Salome","Elene","Nino","Daviti","Lasha","Nikolozi"];
+alert(arrayOfNames[0]);
+let arrayOfSurnames = ["Giorgadze","Chitadze","Iadze","Ninidze","Ninua","Sarishvili"]
+alert(arrayOfSurnames[5]);
+
+let arrayOfColors = ["Blue","Green","Red","Orange","Violet","Indigo","Yellow"];
+alert(arrayOfColors[0]);
+alert(arrayOfColors[1]);
+alert(arrayOfColors[2]);
